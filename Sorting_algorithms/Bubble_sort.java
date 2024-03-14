d6029804120a496f907c0c2a7c5f43dc5ceffe51
Bogdan Klimov <136115919+BogdanKlimov11@users.noreply.github.com>
2024-03-14 17:22:23 +0300

    Update Bubble_sort.java

--- Sorting_algorithms/Bubble_sort.java
+++ Sorting_algorithms/Bubble_sort.java
@@ -1,13 +1,19 @@
 public class Bubble_sort {
     public static void bubble_sort(int[] arr) {
         int n = arr.length;
+        boolean swapped;
         for (int i = 0; i < n - 1; i++) {
+            swapped = false;
             for (int j = 0; j < n - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
+                    swapped = true;
                 }
+            }
+            if (!swapped) {
+                break;
             }
         }
     }
