d191aad574a57aa521bf517a96b2ea1e4b183c2c
Bogdan Klimov <136115919+BogdanKlimov11@users.noreply.github.com>
2024-03-14 17:20:50 +0300

    Create Insertion_sort.java

---
+++ Sorting_algorithms/Insertion_sort.java
@@ -0,0 +1,14 @@
+public class Insertion_sort {
+    public static void insertion_sort(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
+}
